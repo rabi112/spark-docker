57d6f0d7d263667829562e18bb073c3a0f575caf
rabi <rchi478@gmail.com>
2019-08-09 20:12:46 +0530

    modified path

--- docker/spark-submit/spark-submit.sh
+++ docker/spark-submit/spark-submit.sh
@@ -4,7 +4,7 @@ export  BDB_EDGE_HOME='/opt/spark-conf'
 /spark/bin/spark-submit \
 	--verbose \
 	--master ${SPARK_MASTER_URL} \
-	--properties-file /opt/spark-apps/ltts_conf.properties \
+	--properties-file /opt/spark-conf/ltts_conf.properties \
 	--class ${SPARK_APPLICATION_MAIN_CLASS} \
 	--deploy-mode cluster \
 	 ${SPARK_APPLICATION_JAR_LOCATION} 
--- spark-submit/test.sh
+++ spark-submit/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-SPARK_APPLICATION_JAR_LOCATION="/opt/spark-jars/spark-examples_2.11-2.3.0.jar"
-SPARK_APPLICATION_MAIN_CLASS="org.apache.spark.examples.SparkPi"
+SPARK_APPLICATION_JAR_LOCATION="/opt/spark-jars/mqtt-assembly-0.1.jar"
+SPARK_APPLICATION_MAIN_CLASS="com.bdb.mqtt.MQTTSubscriberApp"
 #SPARK_SUBMIT_ARGS="--conf spark.executor.extraJavaOptions='-Dconfig-path=/opt/spark-apps/dev/config.conf'"
 
-docker run  --network dockersparkcluster_default  -v /bdb/conf:/opt/spark-conf -v /bdb/jars:/opt/spark-jars --env SPARK_APPLICATION_JAR_LOCATION=$SPARK_APPLICATION_JAR_LOCATION --env SPARK_APPLICATION_MAIN_CLASS=$SPARK_APPLICATION_MAIN_CLASS spark-submit:latest
+docker run  --network sparkdocker_default  -v /bdb/conf:/opt/spark-conf -v /bdb/jars:/opt/spark-jars --env SPARK_APPLICATION_JAR_LOCATION=$SPARK_APPLICATION_JAR_LOCATION --env SPARK_APPLICATION_MAIN_CLASS=$SPARK_APPLICATION_MAIN_CLASS spark-submit:latest
